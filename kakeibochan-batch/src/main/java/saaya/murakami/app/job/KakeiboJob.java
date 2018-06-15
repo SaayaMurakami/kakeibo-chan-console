e7d234447f5f62da3eedbd52e8a7dcf1b9d9cada
SaayaMurakami <s-murakami@unext.jp>
2018-06-15 17:27:43 +0900

    支出を閲覧・カテゴリ追加・アカウント追加、をできるようにしました

--- kakeibochan-batch/src/main/java/saaya/murakami/app/job/KakeiboJob.java
+++ kakeibochan-batch/src/main/java/saaya/murakami/app/job/KakeiboJob.java
@@ -68,7 +68,7 @@ public class KakeiboJob implements LaJob {
             //家計
             loopout: do {
                 System.out.println("やりたいことを選んでね（数字で選択）");
-                System.out.println("1.家計簿をつける  2.家計簿を見る　3.終了");
+                System.out.println("1.家計簿をつける  2.家計簿を見る　3.カテゴリを編集する　4.アカウントを編集する　5.終了");
                 int num = inputIntNumber("", reader);
 
                 switch (num) {
@@ -76,10 +76,19 @@ public class KakeiboJob implements LaJob {
                     registerStatement(reader, userId);
                     break;
 
-                case 2:
+                case 2://家計簿を見る
+                    showStatement(reader, userId);
                     break;
 
                 case 3:
+                    registerCategory(reader, userId);
+                    break;
+
+                case 4:
+                    registerAccount(reader, userId);
+                    break;
+
+                case 5:
                     System.out.println("--- Bye ---");
                     break loopout;
 
@@ -90,9 +99,146 @@ public class KakeiboJob implements LaJob {
         });
     }
 
+    private void registerCategory(BufferedReader reader, Long userId) {
+        boolean continueAnswer = true;
+        do {
+            String answer;
+            System.out.println("現在のカテゴリー");
+            ListResultBean<Category> categoryList = categoryBhv.selectList(cb -> {
+                cb.query().setUserId_Equal(userId);
+            });
+            if (categoryList.isEmpty()) {
+                System.out.println("カテゴリーは登録されていません");
+            } else {
+                for (int i = 0; i < categoryList.getAllRecordCount(); i++) {
+                    Category category = categoryList.get(i);
+                    System.out.print((i + 1) + ":" + category.getCategory() + " ");
+                }
+                System.out.println("");
+            }
+            System.out.println("何をしますか？");
+            int num;
+            num = inputIntNumber("1.追加　2.削除　3.もどる", reader);
+            switch (num) {
+            case 1:
+                do {
+                    Category category = new Category();
+                    String categoryName = inputString("追加したいカテゴリ名を入力してください", reader);
+                    category.setUserId(userId);
+                    category.setCategory(categoryName);
+                    answer = getAnswer("「" + categoryName + "」" + "を追加しますか？（はい/いいえ）", reader);
+                    if (answer.equals("はい")) {
+                        categoryBhv.insert(category);
+                        System.out.println("追加しました");
+                        break;
+                    } else if (answer.equals("いいえ")) {
+                        System.out.println("やり直します");
+                    }
+                } while (answer.equals("いいえ"));
+                break;
+
+            case 2:
+                System.out.println("ごめんなさい、まだできません！(;_;)");
+                break;
+
+            case 3:
+                continueAnswer = false;
+                break;
+
+            default:
+                break;
+            }
+        } while (continueAnswer);
+    }
+
+    private void registerAccount(BufferedReader reader, Long userId) {
+        boolean continueAnswer = true;
+        do {
+            String answer;
+            System.out.println("現在のアカウント");
+            ListResultBean<Account> accountList = accountBhv.selectList(cb -> {
+                cb.query().setUserId_Equal(userId);
+            });
+            if (accountList.isEmpty()) {
+                System.out.println("アカウントは登録されていません");
+            } else {
+                for (int i = 0; i < accountList.getAllRecordCount(); i++) {
+                    Account account = accountList.get(i);
+                    System.out.print((i + 1) + ":" + account.getAccountName() + " ");
+                }
+                System.out.println("");
+            }
+            System.out.println("何をしますか？");
+            int num;
+            num = inputIntNumber("1.追加　2.削除　3.もどる", reader);
+            switch (num) {
+            case 1:
+                do {
+                    Account account = new Account();
+                    String accountName = inputString("追加したいアカウント名を入力してください", reader);
+                    account.setUserId(userId);
+                    account.setAccountName(accountName);
+                    answer = getAnswer("「" + accountName + "」" + "を追加しますか？（はい/いいえ）", reader);
+                    if (answer.equals("はい")) {
+                        accountBhv.insert(account);
+                        System.out.println("追加しました");
+                        break;
+                    } else if (answer.equals("いいえ")) {
+                        System.out.println("やり直します");
+                    }
+                } while (answer.equals("いいえ"));
+                break;
+            case 2:
+                System.out.println("ごめんなさい、まだできません！(;_;)");
+                break;
+
+            case 3:
+                continueAnswer = false;
+                break;
+
+            default:
+                break;
+            }
+        } while (continueAnswer);
+
+    }
+
+    private void showStatement(BufferedReader reader, Long userId) {
+        ListResultBean<Statement> statementList = statementBhv.selectList(cb -> {
+            cb.query().setUserId_Equal(userId);
+        });
+        if (statementList.isEmpty()) {
+            System.out.println("閲覧するデータはありません");
+        }
+        {
+            statementList.forEach(statement -> {
+                String typeName = null;
+                if (statement.getStatementType().equals("INCOME")) {
+                    typeName = "収入";
+                } else if (statement.getStatementType().equals("SPEND")) {
+                    typeName = "支出";
+                }
+                final String TYPE_NAME = typeName;
+
+                OptionalEntity<Category> category = categoryBhv.selectEntity(cb -> {
+                    cb.query().setCategoryId_Equal(statement.getCategoryId());
+                });
+
+                OptionalEntity<Account> account = accountBhv.selectEntity(cb -> {
+                    cb.query().setAccountId_Equal(statement.getAccountId());
+                });
+
+                System.out.println(TYPE_NAME + " " + statement.getDate() + " " + category.get().getCategory() + " "
+                        + account.get().getAccountName() + " " + statement.getAmount() + "円 " + statement.getMemo());
+            });
+            inputString("閲覧をやめるときはEnterをおす", reader);
+        }
+    }
+
     private void registerStatement(BufferedReader reader, Long userId) {
         do {
             Statement statement = new Statement();
+            statement.setUserId(userId);
 
             //支出or収入を登録
             int typeNum = inputIntNumber("1.支出　2.収入（数字で選択）", reader);
@@ -107,7 +253,7 @@ public class KakeiboJob implements LaJob {
                 typeName = "支出";
             } else if (typeNum == 2) {
                 statementType = "INCOME";
-                typeName = "支出";
+                typeName = "収入";
 
             }
             statement.setStatementType(statementType);
@@ -129,44 +275,45 @@ public class KakeiboJob implements LaJob {
                 cb.query().setUserId_Equal(userId);
                 cb.query().addOrderBy_CategoryId_Asc();
             });
-            categoryList.forEach(category -> {
-                System.out.print(category.getCategoryId() + ":" + category.getCategory() + " ");
-            });
-            Long catergoryNum = inputLongNumber("", reader);
+
+            for (int i = 0; i < countCategory; i++) {
+                Category category = categoryList.get(i);
+                System.out.print((i + 1) + ":" + category.getCategory() + " ");
+            }
+            System.out.println("");
+            int catergoryNum = inputIntNumber("", reader);
             while (catergoryNum < 1 || catergoryNum > countCategory) {
                 System.out.println("1~" + countCategory + "の数字で入力してください");
-                catergoryNum = inputLongNumber("", reader);
+                catergoryNum = inputIntNumber("", reader);
             }
-            final Long CATEGORY_NUM = catergoryNum;
-            OptionalEntity<Category> category = categoryBhv.selectEntity(cb -> {
-                cb.query().setCategoryId_Equal(CATEGORY_NUM);
-            });
-            String categoryName = category.get().getCategory();
-            statement.setCategoryId(CATEGORY_NUM);
+            Category category = categoryList.get(catergoryNum - 1); //選択されたカテゴリーを取得
+            String categoryName = category.getCategory();
+            Long categoryId = category.getCategoryId();
+            statement.setCategoryId(categoryId);
 
             //アカウントを登録
             System.out.println("アカウントを選んでください（数字で選択）");
             int countAccount = accountBhv.selectCount(cb -> {
                 cb.query().setUserId_Equal(userId);
             });
-            ListResultBean<Account> AccounttList = accountBhv.selectList(cb -> {
+            ListResultBean<Account> accountList = accountBhv.selectList(cb -> { //ログインユーザーに該当するアカウントをリストに格納
                 cb.query().setUserId_Equal(userId);
                 cb.query().addOrderBy_AccountId_Asc();
             });
-            AccounttList.forEach(account -> {
-                System.out.print(account.getAccountId() + ":" + account.getAccountName() + " ");
-            });
-            Long accountNum = inputLongNumber("", reader);
+            for (int i = 0; i < countAccount; i++) {
+                Account account = accountList.get(i);
+                System.out.print((i + 1) + ":" + account.getAccountName() + " ");
+            }
+            System.out.println("");
+            int accountNum = inputIntNumber("", reader);
             while (accountNum < 1 || accountNum > countAccount) {
                 System.out.println("1~" + countAccount + "の数字で入力してください");
-                accountNum = inputLongNumber("", reader);
+                accountNum = inputIntNumber("", reader);
             }
-            final Long ACCOUNT_NUM = accountNum;
-            OptionalEntity<Account> selectMember2 = accountBhv.selectEntity(cb -> {
-                cb.query().setAccountId_Equal(ACCOUNT_NUM);
-            });
-            String accountName = selectMember2.get().getAccountName();
-            statement.setAccountId(ACCOUNT_NUM);
+            Account account = accountList.get(accountNum - 1); //選択されたカテゴリーを取得
+            String accountName = account.getAccountName();
+            Long accountId = account.getAccountId();
+            statement.setAccountId(accountId);
 
             //金額を登録
             int amount = inputIntNumber("金額を入力してください（ex.500）", reader);
@@ -196,7 +343,7 @@ public class KakeiboJob implements LaJob {
     private long loginProcess(BufferedReader reader) {
         do {
             System.out.println("家計簿ちゃん");
-            System.out.println("1.会員登録　2.ログイン");
+            System.out.println("1.会員登録　2.ログイン 3.終了");
             int selectNum = inputIntNumber("", reader);
             switch (selectNum) {
             case 1: //会員登録
@@ -205,32 +352,53 @@ public class KakeiboJob implements LaJob {
                 String userName;
                 String password;
                 do {
-                    mailAddress = inputString("メールアドレスを入力してください", reader);
+                    loop: do {
+                        mailAddress = inputString("メールアドレスを入力してください", reader);
+                        final String MAIL_ADDRESS = mailAddress;
+                        OptionalEntity<Member> selectMember = memberBhv.selectEntity(cb -> {
+                            cb.query().setMailAddress_Equal(MAIL_ADDRESS);
+                        });
+                        boolean Existence = selectMember.isPresent();
+                        if (Existence) {
+                            System.out.println("入力されたメールアドレスはすでに使われています");
+                        } else {
+                            break loop;
+                        }
+                    } while (true);
+
                     userName = inputString("ユーザー名を入力してください", reader);
                     password = inputString("パスワードを入力してください", reader);
                     System.out.println("入力情報確認");
                     System.out.println("メールアドレス：" + mailAddress);
-                    System.out.print("ユーザー名：" + userName);
+                    System.out.println("ユーザー名：" + userName);
                     System.out.println("パスワード：" + password);
                     loginAnswer = getAnswer("入力間違いはありませんか？（はい/いいえ）", reader);
                     if (loginAnswer.equals("いいえ")) {
                         System.out.println("やり直します");
                     }
                 } while (loginAnswer.equals("いいえ"));
-
                 Member member = new Member();
                 member.setMailAddress(mailAddress);
                 member.setUserName(userName);
                 member.setPassword(password);
                 memberBhv.insert(member);
+
+                Long userId = member.getUserId();
+                Category category = new Category();
+                category.setUserId(userId);
+                category.setCategory("食費");
+                categoryBhv.insert(category);
+
+                Account account1 = new Account();
+                account1.setUserId(userId);
+                account1.setAccountName("財布");
+                accountBhv.insert(account1);
+                Account account2 = new Account();
+                account2.setUserId(userId);
+                account2.setAccountName("銀行");
+                accountBhv.insert(account2);
+
                 System.out.println("登録完了");
-
-                memberBhv.selectList(cb -> {
-                    cb.query().addOrderBy_UserId_Asc();
-                }).forEach(tmpMember -> {
-                    System.out.println(tmpMember.toString());
-                });
-
                 break;
 
             case 2:
@@ -248,6 +416,10 @@ public class KakeiboJob implements LaJob {
                         System.out.println("入力し直してください");
                     }
                 } while (true);
+
+            case 3:
+                System.out.println("--- bye ---");
+                System.exit(0);
 
             default:
                 break;
